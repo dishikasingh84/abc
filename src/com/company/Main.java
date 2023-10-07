b4871c816d2e6d99cc617c710ff5b7e80124938b
dishikasingh84 <dishikasingh84@gmail.com>
2023-10-07 19:44:18 +0530

    Demo commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int i=10;
-	System.out.println("i");
-	System.out.println(i);
+	String s="Upgrad_campus";
+	System.out.println("s");
+	System.out.println(s);
     }
 }
